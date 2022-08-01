253e210de1d8ddcccf6c28210283db5d2a65a989
SergeyGrigorev <sergey7787@mail.ru>
2022-08-02 00:10:28 +0300

    Addition 2 void: -whatSeason -writeSeason

---
+++ qa_guru_Git1/qa_guru_Lesson2/src/main/java/Grigorev/Date.java
@@ -0,0 +1,11 @@
+package Grigorev;
+
+public class Date {
+
+    public static void main(String[] args) {
+        Season first_date = new Season(11, 10);
+        first_date.whatSeason();
+        first_date.writeSeason();
+    }
+
+}
---
+++ qa_guru_Git1/qa_guru_Lesson2/src/main/java/Grigorev/Season.java
@@ -0,0 +1,23 @@
+package Grigorev;
+public class Season {
+    int date;
+    int month;
+    String season;
+    
+    public Season(int date, int month) {
+        this.date = date;
+        this.month = month;
+    }
+    void whatSeason() {
+        if ((month == 12 ) || (month < 3 )) {season = "Зима!";}
+        if ((month > 2 ) && (month < 6 )) {season = "Весна!";}
+        if ((month > 5 ) && (month < 9 )) {season = "Лето!";}
+        if ((month > 8 ) && (month < 12 )) {season = "Осень!";}
+    }
+    void writeSeason () {
+            if (month <= 12 && month >=1 ) {
+                System.out.println(date + "." + month + " - это " + season);
+            }
+            else {System.out.println("Введите корректную дату!");}
+        }
+    }
